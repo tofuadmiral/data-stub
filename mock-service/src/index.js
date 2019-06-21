cdd41eaf96d8da7750013177946772be311406c6
tofuadmiral <fuad98ali@gmail.com>
2019-06-21 11:43:17 -0400

    added index.js entry point

---
+++ mock-service/src/index.js
@@ -0,0 +1,10 @@
+const mb = require('mountebank');
+const settings = require('./settings');
+
+const mbServerInstance = mb.create({
+        port: settings.port,
+        pidfile: '../mb.pid',
+        logfile: '../mb.log',
+        protofile: '../protofile.json',
+        ipWhitelist: ['*']
+    });
